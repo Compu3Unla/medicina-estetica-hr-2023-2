4101af98b9b419d32529e383e6ebe8b49402aa7d
DaianaParisi <daii_27_@hotmail.com>
2023-11-03 02:25:26 -0300

    20off  time transition

--- 20off.js
+++ 20off.js
@@ -1,9 +1,14 @@
-// JavaSc
+// JavaScript
+       
+        function mostrarModalDespuesDeTiempo() {
+            const miModal = document.getElementById("miModal");
+            miModal.classList.add("mostrando");
+			miModal.showModal();
+        }
 
-        const miModal = document.getElementById("miModal");
-        miModal.showModal();
-
+        setTimeout(mostrarModalDespuesDeTiempo, 2000);
+        
         const cerrarModalBtn = document.getElementById("cerrarModal");
         cerrarModalBtn.addEventListener("click", () => {
-            miModal.close();
+        miModal.close();
         });
